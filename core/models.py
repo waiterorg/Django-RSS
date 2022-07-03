339f6d7e969848d46b1cc698e64cd91ebaa0724d
waiterorg <iwaiterorg@gmail.com>
2022-07-03 16:54:37 +0430

    Edit models

--- core/models.py
+++ core/models.py
@@ -2,28 +2,36 @@ from django.db import models
 from django.utils import timezone
 
 
-class Rss(models.Model):
-    """ 
-    store rss urls
-    """
-    name = models.CharField()
-    url = models.URLField()
+class TimeStamp(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
+    class Meta:
+        abstract = True
+
+
+class Rss(TimeStamp):
+    """
+    store rss urls
+    """
+
+    name = models.CharField()
+    url = models.URLField()
+    status = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.name} in {self.url} RSS"
 
-class News(models.Model):
-    """ 
-    store news article 
+
+class News(TimeStamp):
     """
-    title = models.CharField(max_length = 200) 
+    store news article
+    """
+
+    title = models.CharField(max_length=200)
     link = models.URLField()
     description = models.TextField()
     creator = models.CharField(blank=True, null=True)
-    published = models.DateTimeField(default = timezone.now)
-    
-    
+
     def __str__(self):
         return self.title
