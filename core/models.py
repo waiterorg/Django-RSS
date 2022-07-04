5327817d20aee9210c20328628c1c537c84edcbc
waiterorg <iwaiterorg@gmail.com>
2022-07-04 15:54:50 +0430

    Complete rss and news models

--- core/models.py
+++ core/models.py
@@ -15,9 +15,12 @@ class Rss(TimeStamp):
     store rss urls
     """
 
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     url = models.URLField()
     status = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "RSS Urls"
 
     def __str__(self):
         return f"{self.name} in {self.url} RSS"
@@ -31,7 +34,11 @@ class News(TimeStamp):
     title = models.CharField(max_length=200)
     link = models.URLField()
     description = models.TextField()
-    creator = models.CharField(blank=True, null=True)
+    creator = models.CharField(max_length=200, blank=True, null=True)
+    published = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "News Articles"
 
     def __str__(self):
         return self.title
