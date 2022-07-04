cf141d2a2b55ddcff64e83634b1bbca33c93694e
waiterorg <iwaiterorg@gmail.com>
2022-07-04 16:38:49 +0430

    setup page number pagination

--- rss_grabber/settings.py
+++ rss_grabber/settings.py
@@ -126,5 +126,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
 CELERY_BROKER_URL = "redis://redis:6379"
 CELERY_RESULT_BACKEND = "redis://redis:6379"
