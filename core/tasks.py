e03ffbe3111585a767b374e0f01398e96f5c822f
waiterorg <iwaiterorg@gmail.com>
2022-07-04 17:42:55 +0430

    error handling

--- core/tasks.py
+++ core/tasks.py
@@ -11,25 +11,26 @@ from .models import News, Rss
 logger = get_task_logger(__name__)
 
 
-@shared_task
+@shared_task()
 def get_news():
     try:
         rss_list = Rss.objects.filter(status=True)
-        for rss in rss_list:
-            url = rss.url
-            feed = feedparser.parse(url)
-            if feed["entries"]:
-                for entry in feed["entries"]:
-                    news, created = News.objects.get_or_create(
-                        title=entry["title"],
-                        published=datetime.fromtimestamp(
-                            mktime(entry["published_parsed"])
-                        ),
-                    )
-                    if created:
-                        news.link = entry["link"]
-                        news.description = entry["summary"]
-                        news.creator = entry["author"]
-                        news.save()
+        if rss_list.exists():
+            for rss in rss_list:
+                url = rss.url
+                feed = feedparser.parse(url)
+                if feed["entries"]:
+                    for entry in feed["entries"]:
+                        news, created = News.objects.get_or_create(
+                            title=entry["title"],
+                            published=datetime.fromtimestamp(
+                                mktime(entry["published_parsed"])
+                            ),
+                        )
+                        if created:
+                            news.link = entry["link"]
+                            news.description = entry["summary"]
+                            news.creator = entry["author"]
+                            news.save()
     except:
         logger.error("failed, there was an issue !")
