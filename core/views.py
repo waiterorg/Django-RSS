4716e741ea7f89a7d3b25efd655f42bab92c2767
waiterorg <iwaiterorg@gmail.com>
2022-07-04 15:56:55 +0430

    Add updatenews api for active update news from rss urls and newslist for listing news

--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import News
+from .serializers import NewsSerializer
+from .tasks import get_news
+
+
+class UpdateNews(APIView):
+    def post(self, request):
+        get_news.delay()
+        return Response({"msg": "ok"})
+
+
+class NewsList(APIView):
+    """
+    Return list of all news with pagination
+    """
+
+    # TODO:
+    # 1. [] jwt auth permision for access
+    # 2. [] Create serializer
+    # 3. [] drf pagination
+    def get(self, request):
+        queryset = News.objects.all()
+        serializer = NewsSerializer(queryset, many=True)
+        return Response(serializer.data)
